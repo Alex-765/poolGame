6cd54d0d31c09f0657e4285179ca112f9346a2b2
Alex Bater <bater.a@etoncollege.org.uk>
2023-09-13 15:40:43 +0100

    Initial Commit

---
+++ poolGame/poolGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  poolGameApp.swift
+//  poolGame
+//
+//  Created by Bater, Alexander (IRG) on 13/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct poolGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
